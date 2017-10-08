c9163a6e427c8c6a9dcd65868e590a7bf4313970
Sergey Novikov <novikov359@gmail.com>
2017-10-08 12:32:42 +0200

    Refactor tokenizer using bufio

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"io/ioutil"
@@ -37,9 +38,8 @@ func init() {
 	}
 
 	ClassRules = map[string]*regexp.Regexp{
-		"int64":  regexp.MustCompile(`^[0-9]+$`),
-		"bool":   regexp.MustCompile(`^(true|false)$`),
-		"string": regexp.MustCompile(`^\".+\"$`),
+		"int64": regexp.MustCompile(`^[0-9]+$`),
+		"bool":  regexp.MustCompile(`^(true|false)$`),
 	}
 }
 
@@ -76,25 +76,81 @@ type Node struct {
 }
 
 func tokenize(src string) []string {
-	src = strings.Replace(src, "(", " ( ", -1)
-	src = strings.Replace(src, ")", " ) ", -1)
-
 	var tokens []string
+	var char rune
+	var err error = nil
+	var inString bool = false
+	var atom []rune
 
-	for _, token := range strings.Split(src, " ") {
-		if token != "" && token != "\n" && token != "\n\n" {
-			tokens = append(tokens, token)
+	reader := bufio.NewReader(strings.NewReader(src))
+
+	for err == nil {
+		char, _, err = reader.ReadRune()
+
+		if inString {
+			if isQUOTE(char) {
+				inString = false
+				tokens = append(tokens, string(atom))
+				atom = atom[:0]
+			} else {
+				atom = append(atom, char)
+			}
+
+			continue
+		}
+
+		switch {
+		case isLPAR(char):
+			tokens = append(tokens, string(char))
+		case isRPAR(char):
+			applyAtom(&tokens, &atom)
+			tokens = append(tokens, string(char))
+		case isLITERAL(char):
+			atom = append(atom, char)
+		case isINTEGER(char):
+			atom = append(atom, char)
+		case isQUOTE(char):
+			inString = true
+		case isWHITESPACE(char):
+			applyAtom(&tokens, &atom)
 		}
 	}
 
 	return tokens
 }
 
-func parse(tokens []string) (Node, int) {
-	if Debug {
-		fmt.Println(tokens)
-	}
+func isLPAR(r rune) bool {
+	return r == '('
+}
 
+func isRPAR(r rune) bool {
+	return r == ')'
+}
+
+func isWHITESPACE(r rune) bool {
+	return r == ' ' || r == '\n'
+}
+
+func isQUOTE(r rune) bool {
+	return r == '"'
+}
+
+func isINTEGER(r rune) bool {
+	return r >= '0' && r <= '9'
+}
+
+func isLITERAL(r rune) bool {
+	return (r >= 'a' && r <= 'z') || (r >= 'A' && r <= 'Z') || r == '=' || r == '%'
+}
+
+func applyAtom(tokens *[]string, atom *[]rune) {
+	if len(*atom) > 0 {
+		*tokens = append(*tokens, string(*atom))
+		*atom = (*atom)[:0]
+	}
+}
+
+func parse(tokens []string) (Node, int) {
 	var token string
 	var size int = len(tokens)
 	var i int = 0
@@ -158,15 +214,11 @@ func parseAtom(node Node) interface{} {
 	switch class {
 	case "int64":
 		var _value int64
-		_value, _ = strconv.ParseInt(node.atom, 10, 32)
+		_value, _ = strconv.ParseInt(node.atom, 10, 64)
 		return _value
 	case "bool":
 		var _value bool
 		_value, _ = strconv.ParseBool(node.atom)
-		return _value
-	case "string":
-		var _value string
-		_value, _ = strconv.Unquote(node.atom)
 		return _value
 	default:
 		return node.atom
